58c84c3ad8fda3a5666adcb915d794b3139bb7fe
Rémy HUBSCHER <remy@chefclub.tv>
2018-04-26 11:05:48 +0200

    Back to development: 1.4.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ REQUIREMENTS = [
 ]
 
 setup(name='kinto-redis',
-      version='1.3.0',
+      version='1.4.0.dev0',
       description='Kinto Redis',
       long_description=README + '\r\n' + CHANGELOG,
       license='Apache License (2.0)',
